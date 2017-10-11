de51eb1b7b813e4fccd2ef7dc2ed5107ed93314d
Kieran Jacobsen <code@powershell.com>
2017-10-11 20:59:29 +1100

    Switched to module.psm1 from http://overpoweredshell.com/Working-with-Plaster/ as it seems to be rather effient (even more than my previous versions).

--- module.psm1
+++ module.psm1
@@ -1,35 +1,21 @@
-[cmdletbinding()]
-param()
-Write-Verbose $PSScriptRoot
-
-Write-Verbose 'Import everything in sub folders folder'
-
-foreach($folder in @('internal', 'functions'))
-
-{
-
-    $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
-
-    if(Test-Path -Path $root)
-
-    {
-
-        Write-Verbose "processing folder $root"
-
-        $files = Get-ChildItem -Path $root -Filter *.ps1 -Recurse
-
-
-
-        # dot source each file
-
-        $files | where-Object{ $_.name -NotLike '*.Tests.ps1'} | 
-
-            ForEach-Object{Write-Verbose $_.basename; . $_.FullName}
-
-    }
-
-}
-
-
+# Taken from http://overpoweredshell.com/Working-with-Plaster/
 
 Export-ModuleMember -function (Get-ChildItem -Path "$PSScriptRoot\functions\*.ps1").basename
+
+$functionFolders = @('functions', 'internal', 'classes')
+ForEach ($folder in $functionFolders)
+{
+    $folderPath = Join-Path -Path $PSScriptRoot -ChildPath $folder
+    If (Test-Path -Path $folderPath)
+    {
+        Write-Verbose -Message "Importing from $folder"
+        $functions = Get-ChildItem -Path $folderPath -Filter '*.ps1'
+        ForEach ($function in $functions)
+        {
+            Write-Verbose -Message "  Importing $($function.BaseName)"
+            . $($function.FullName)
+        }
+    }
+}
+$publicFunctions = (Get-ChildItem -Path "$PSScriptRoot\functions" -Filter '*.ps1').BaseName
+Export-ModuleMember -Function $publicFunctions
