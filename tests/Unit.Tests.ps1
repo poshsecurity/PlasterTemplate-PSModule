5cded1708206ee35e50f4766cca63de3f79c0c14
Kieran Jacobsen <code@poshsecurity.com>
2017-10-15 13:45:56 +1100

    Unitr tests now have correct ModuleScope

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -1,14 +1,27 @@
 $script:ModuleName = '<%= $PLASTER_PARAM_ModuleName %>'
+
 # Removes all versions of the module from the session before importing
 Get-Module $ModuleName | Remove-Module
+
 $ModuleBase = Split-Path -Parent $MyInvocation.MyCommand.Path
+
 # For tests in .\Tests subdirectory
 if ((Split-Path $ModuleBase -Leaf) -eq 'Tests') {
     $ModuleBase = Split-Path $ModuleBase -Parent
 }
+
 ## this variable is for the VSTS tasks and is to be used for refernecing any mock artifacts
 $Env:ModuleBase = $ModuleBase
+
 Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
-Describe "Basic function unit tests" -Tags Build , Unit{
+
+# InModuleScope runs the test in module scope.
+# It creates all variables and functions in module scope.
+# As a result, test has access to all functions, variables and aliases
+# in the module even if they're not exported.
+InModuleScope $script:ModuleName {
+    Describe "Basic function unit tests" -Tags Build , Unit{
+
+    }
 
 }
